698b13001fcf68193e078c4946b605fcdaf8ab7e
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-12-28 22:42:40 +0600

    fetch data from server

--- src/components/FormsData.jsx
+++ src/components/FormsData.jsx
@@ -1,61 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import product from "../assets/product/productOne.jpg";
+import Coding from "../assets/product/coding.jpg"
 
 const FormsData = () => {
+  let [title, setTitle] = useState("");
+  let [description, setDescription] = useState("");
+  let [badgeText, setBadgeText] = useState("");
+  let [badgeColor, setBadgeColor] = useState("");
+  let [instructorName, setInstructorName] = useState("");
+  let [dataList, setDataList] = useState([]);
 
-  let [title, setTitle]=useState("");
-  let [description, setDescription]=useState("");
-  let [badgeText, setBadgeText]=useState("");
-  let [badgeColor, setBadgeColor]=useState("");
-  let [instructorName, setInstructorName]=useState("");
-
-
-  let handleTitle =(e)=>{
+  let handleTitle = (e) => {
     setTitle(e.target.value);
-  }
-  let handleDescription =(e)=>{
+  };
+  let handleDescription = (e) => {
     setDescription(e.target.value);
-  }
+  };
 
-  let handleBadgeText =(e)=>{
+  let handleBadgeText = (e) => {
     setBadgeText(e.target.value);
-  }
+  };
 
-  let handleBadgeColor = (e)=>{
+  let handleBadgeColor = (e) => {
     setBadgeColor(e.target.value);
-  }
+  };
 
-  let handleInstructorName = (e)=>{
-
+  let handleInstructorName = (e) => {
     setInstructorName(e.target.value);
-  }
+  };
 
-   let token = JSON.parse(localStorage.getItem("authToken"));
+  let token = JSON.parse(localStorage.getItem("authToken"));
 
-   
-   let navigate = useNavigate();
+  let navigate = useNavigate();
 
-  useEffect(()=>{
-
-    if(!token){
-      navigate('/login');
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
     }
+  }, []);
 
-  },[])
-
-  let handleSubmit =async (e)=>{
+  let handleSubmit = async (e) => {
     e.preventDefault();
 
-
- 
     try {
       let courseData = {
         title: title,
         description: description,
         badge_text: badgeText,
         badge_color: badgeColor,
-        instructor_name:instructorName
+        instructor_name: instructorName,
       };
       let postData = await axios.post(
         "https://react-interview.crd4lc.easypanel.host/api/course",
@@ -65,77 +60,90 @@ const FormsData = () => {
             Accept: "application/json",
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`
-
           },
         }
       );
       alert("Successfully Submitted");
       console.log(postData);
-      
 
       // setTimeout(() => {
       //   navigate("/login");
       // }, 2000);
     } catch (error) {
       console.log(error);
-    } 
- 
+    }
+  };
 
-  }
-  
+  useEffect(() => {
+    let fetchData = async () => {
+      try {
+        let dataCourses = await axios.get(
+          "https://react-interview.crd4lc.easypanel.host/api/course",
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
-  // let dataCourses = axios.get('')
+        setDataList(dataCourses.data.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
+  console.log(dataList);
 
   return (
-    <section className='bg-[url("./assets/background/backgroundFour.jpg")]'>
-      <div className="container mx-auto flex justify-center items-center w-[100vw] h-[100vh]">
-        <div className="bg-[#d8d4cf59] rounded-lg h-[500px] w-[700px]  flex justify-center items-center flex-col">
-          <h1 className="text-3xl font-bold py-3 underline">Add Course  Data</h1>
+    // bg-[url("./assets/background/backgroundFour.jpg")]
+    <section className="">
+      <div className="flex justify-center items-center w-[100vw] py-12">
+        <div className="bg-[#d8d4cf59] rounded-lg h-[500px] w-[700px]  flex justify-center items-center flex-col ">
+          <h1 className="text-3xl font-bold py-3 underline">Add Course Data</h1>
           <form action="" method="POST">
             <div>
               <input
-              onChange={handleTitle}
+                onChange={handleTitle}
                 className="outline-none px-6 py-3 mt-2 rounded-lg"
                 placeholder="Title"
                 type="text"
-                name=""
-                id=""
+              
               />
             </div>
             <div>
               <input
-              onChange={handleDescription}
+                onChange={handleDescription}
                 className="outline-none px-6 py-3 mt-2 rounded-lg"
                 placeholder="Description"
                 type="text"
-                name=""
-                id=""
+                
               />
             </div>
             <div>
               <input
-              onChange={handleBadgeText}
+                onChange={handleBadgeText}
                 className="outline-none px-6 py-3 mt-2 rounded-lg"
                 placeholder="Badge Text"
                 type="text"
-                name=""
-                id=""
+                
               />
             </div>
             <div>
               <input
-              onChange={handleBadgeColor}
+                onChange={handleBadgeColor}
                 className="outline-none px-6 py-3 mt-2 rounded-lg"
                 placeholder="Badge Color"
                 type="text"
-                name=""
-                id=""
+               
               />
             </div>
             <div>
               <input
-              onChange={handleInstructorName}
+                onChange={handleInstructorName}
                 className="outline-none px-6 py-3 mt-2 rounded-lg"
                 placeholder="Instructor Name"
                 type="text"
@@ -143,19 +151,40 @@ const FormsData = () => {
                 id=""
               />
             </div>
-            
+
             <div className="mt-3 flex justify-center">
-              <button onClick={handleSubmit} className="bg-[#ececea] px-10 py-2 rounded-md font-bold">
+              <button
+                onClick={handleSubmit}
+                className="bg-[#ececea] px-10 py-2 rounded-md font-bold"
+              >
                 Submit
               </button>
             </div>
           </form>
         </div>
 
-        <div>
-
-        </div>
+        
       </div>
+      <h3 className="mt-20  text-xl md:text-4xl text-center font-bold mb-8">The List Of Courses</h3>
+      <div className="w-[100%] flex flex-wrap justify-center items-center gap-4 mt-3">
+        
+          {dataList.map((item) => (
+            <div className="w-[29%] shadow-lg rounded-xl  bg-gray-100">
+              <div>
+                            <img
+                              src={Coding}
+                              alt=""
+                              className="h-[350px] w-full"
+                            />
+                          </div>
+              <p  className="text-3xl text-center font-bold">Title: {item.title}</p>
+              <p className="text-2xl text-center ">Description: {item.description}</p>
+              <p className="text-2xl text-center ">Badge_text: {item.badge_text}</p>
+              <p className="text-2xl text-center">Badge_color: {item.badge_color}</p>
+              <p className="text-2xl text-center">Instructor_name{item.instructor_name}</p>
+            </div>
+          ))}
+        </div>
     </section>
   );
 };
